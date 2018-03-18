e0376d5640acce98936b471b7b402a49854e00be
WTobor <weronika.tobor@op.pl>
2018-03-18 21:24:38 +0100

    Fibonacci algorithm

---
+++ Challenges.Tests/FibonacciTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class FibonacciTest
+    {
+        private readonly int[] _expectedResult =
+        {
+            2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181
+        };
+
+
+        [Fact]
+        public void FibonacciFrom0()
+        {
+            var result = Fibonacci.GetFibValue(0);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void FibonacciFrom1()
+        {
+            var result = Fibonacci.GetFibValue(1);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void FibonacciFrom2()
+        {
+            var result = Fibonacci.GetFibValue(2);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void FibonacciFrom3()
+        {
+            var result = Fibonacci.GetFibValue(3);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void FibonacciFromNegative()
+        {
+            var result = new List<int>();
+            foreach (var input in Enumerable.Range(3, 17))
+                result.Add(Fibonacci.GetFibValue(input));
+            Assert.Equal(_expectedResult.ToList(), result);
+        }
+    }
+}
---
+++ Challenges/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Challenges
+{
+    /// INPUT
+    /// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10...
+    /// LOGIC
+    /// if number is 0, then return 0
+    /// if number is 1 or 2, then return 1
+    /// if number is bigger than 2, then return Fib(n-1) + Fib(n-2)
+    /// OUTPUT
+    /// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...
+    public static class Fibonacci
+    {
+        public static int GetFibValue(int value)
+        {
+            if (value < 0)
+                throw new FormatException();
+            if (value == 0)
+                return 0;
+            if (value < 2)
+                return 1;
+            return GetFibValue(value - 1) + GetFibValue(value - 2);
+        }
+    }
+}
